b6a22ab92b65395905c11e9cbd7a862bbc4cdab5
Mahasak Pijittum <mahasak.pijittum@agoda.com>
2016-10-08 13:25:17 +0700

    refactor metric control #2

---
+++ RealtimeMeasurement.Infrastructure/Metrics/IWebsiteMetric.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealtimeMeasurement.Infrastructure.Metrics
+{
+    public interface IWebsiteMetric
+    {
+        void markWhenCustomerInterest(string item);
+        void markWhenCustomerBooking(string item);
+        void markWhenCustomerMadePayment(string item);
+        void markWhenCustomerCancel(string item);
+        void markWhenCustomerOnHomePage();
+        void markWhenCustomerOnProductPage();
+        void markWhenCustomerOnCartPage();
+    }
+}
---
+++ RealtimeMeasurement.Infrastructure/Metrics/WebsiteMetric.cs
@@ -0,0 +1,69 @@
+﻿using Metrics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealtimeMeasurement.Infrastructure.Metrics
+{
+    public class WebsiteMetric: IWebsiteMetric
+    {
+        private readonly Counter interest = Metric.Counter("Interest", Unit.Calls);
+
+        private readonly Counter booking = Metric.Counter("Booking", Unit.Calls);
+
+        private readonly Counter payment = Metric.Counter("Payment", Unit.Calls);
+
+        private readonly Counter cancel = Metric.Counter("Cancel", Unit.Calls);
+
+        private readonly Counter home = Metric.Counter("OnHomePage", Unit.Calls);
+
+        private readonly Counter product = Metric.Counter("OnProductPage", Unit.Calls);
+
+        private readonly Counter cart = Metric.Counter("OnCartPage", Unit.Calls);
+
+        public WebsiteMetric Instance
+        {
+            get
+            {
+                return new WebsiteMetric();
+            }
+        }
+
+        public void markWhenCustomerInterest(string item)
+        {
+            interest.Increment(item);
+        }
+
+        public void markWhenCustomerBooking(string item)
+        {
+            booking.Increment(item);
+        }
+
+        public void markWhenCustomerMadePayment(string item)
+        {
+            payment.Increment(item);
+        }
+
+        public void markWhenCustomerCancel(string item)
+        {
+            cancel.Increment(item);
+        }
+
+        public void markWhenCustomerOnHomePage()
+        {
+            home.Increment();
+        }
+
+        public void markWhenCustomerOnProductPage()
+        {
+            product.Increment();
+        }
+
+        public void markWhenCustomerOnCartPage()
+        {
+            cart.Increment();
+        }
+    }
+}
